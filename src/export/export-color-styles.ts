bf7f67b0e27a6ea14358af30352cf1beec83421a
Gwen Wendell <gwen@thecodingdesigner.com>
2025-04-28 11:00:09 -0400

    Refactors color token export logic
    Simplifies the JSON structure setup by importing tokens directly.
    Introduces theme detection based on the Figma file name.
    Adjusts logic to dynamically assign paint styles to the detected theme.

--- src/export/export-color-styles.ts
+++ src/export/export-color-styles.ts
@@ -1,4 +1,23 @@
+import tokensImport from "../tokens.json";
+
 const exportColorStyles = async () => {
+  // Set up the JSON object structure
+  const tokensJSON: {
+    "color-tokens": {
+      dark: Record<string, unknown>;
+      light: Record<string, unknown>;
+    };
+  } = {
+    "color-tokens": {
+      dark: tokensImport["color-tokens"]["dark"],
+      light: tokensImport["color-tokens"]["light"],
+    },
+  };
+
+  // sort color themes into respective json sections
+  const figmaFileName = figma.root.name;
+  const themename = figmaFileName.includes("light") ? "light" : "dark";
+
   // Get all local paint styles
   const paintStyles = await figma.getLocalPaintStylesAsync().then(
     (paintStyles) => paintStyles,
@@ -7,14 +26,6 @@ const exportColorStyles = async () => {
       return [];
     }
   );
-
-  // Set up the JSON object structure
-  const tokensJSON = {
-    "color-tokens": {
-      [ "dark" ]: {},
-      [ "light" ]: {},
-    },
-  };
 
   // Loop through the paint styles and add them to a JSON object
   const paintStylesJSON: {
@@ -39,9 +50,9 @@ const exportColorStyles = async () => {
     };
   });
 
-  tokensJSON["color-tokens"]["dark"] = paintStylesJSON;
+  tokensJSON["color-tokens"][themename] = paintStylesJSON;
   // Send JSON to the UI
   figma.ui.postMessage({ type: "exportJSON", content: tokensJSON });
 };
 
-export { exportColorStyles}
+export { exportColorStyles };

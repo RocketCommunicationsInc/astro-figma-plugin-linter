091f4524bbc9c8e7a278d9f22ed5a10833c7ffd9
Gwen Wendell <gwen@thecodingdesigner.com>
2025-04-30 16:58:20 -0400

    Removes unused export functions and updates message handling
    Eliminates unused color and type export functions to streamline code.
    Introduces a new lint-selection message handler for enhanced functionality.
    
    Improves maintainability by reducing unused code.

--- src/code.ts
+++ src/code.ts
@@ -1,6 +1,3 @@
-import { exportColorStyles } from "./export/export-color-styles";
-import { exportTypeStyles } from "./export/export-type-styles";
-
 figma.showUI(__html__, { themeColors: true, width: 650, height: 525 });
 
 // Calls to "parent.postMessage" from within the HTML page will trigger this
@@ -10,14 +7,8 @@ figma.ui.onmessage = (msg: { type: string; count: number }) => {
   // One way of distinguishing between different types of messages sent from
   // your HTML page is to use an object with a "type" property like this.
 
-  if (msg.type === "export-color") {
-    figma.notify("Exporting color styles...");
-    exportColorStyles();
-  }
-
-  if (msg.type === "export-type") {
-    figma.notify("Exporting text styles...");
-    exportTypeStyles();
+  if (msg.type === "lint-selection") {
+    figma.notify("Linting Selection...");
   }
 
   if (msg.type === "cancel") {

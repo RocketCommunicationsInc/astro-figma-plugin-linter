eb588ad29aa91d60aba35d0c146831ba4e53d274
Gwen Wendell <gwen@thecodingdesigner.com>
2025-05-09 17:53:07 -0400

    Adds support for component tokens
    Introduces a new map to handle component-specific tokens,
    extending the token system to include component entries.
    Enhances flexibility and reusability of token management.
    
    No related issue mentioned.

--- src/tokens/index.ts
+++ src/tokens/index.ts
@@ -9,9 +9,11 @@ const tokens = () => {
   const tokens = tokensJson as TokensJSON;
   const colors = tokens['color-tokens'];
   const type = tokens['type-tokens'];
+  const components = tokens['components'];
 
   const colorTokens = new Map();
   const typeTokens = new Map();
+  const astroComponents = new Map();
   Object.entries(colors.dark).forEach(([key, value]) => {
     const nameWithTheme = 'dark/' + key;
     const styleKey = (value as { key: string })['key'];
@@ -29,10 +31,16 @@ const tokens = () => {
     typeTokens.set(key, value);
     typeTokens.set(styleKey, value);
   });
+  Object.entries(components).forEach(([key, value]) => {
+    const componentKey = (value as { key: string })['key'];
+    astroComponents.set(key, value);
+    astroComponents.set(componentKey, value);
+  });
 
   return {
     colorTokens,
     typeTokens,
+    astroComponents
   };
 }
 

99107f0073d0e6b91b74d59e5c9cb3068ef0264e
Gwen Wendell <gwen@thecodingdesigner.com>
2025-05-19 13:55:34 -0400

    Improves theme validation for Astro color usage
    Enhances logic to handle cases where Astro color is undefined, providing a clearer message for non-Astro fill styles. Refactors validation for better readability and accuracy.

--- src/lint/colors/tests/astro-color-is-using-correct-theme.ts
+++ src/lint/colors/tests/astro-color-is-using-correct-theme.ts
@@ -13,12 +13,18 @@ const astroColorIsUsingCorrectTheme = (
     const name = node.name;
     const fillStyleId = (node.fillStyleId as string) || "";
     const astroColor = colorTokens.get(stripToLoadableId(fillStyleId));
-    const astroColorNameWithTheme = `${theme}/${astroColor?.name}`;
-    const astroColorWithTheme = colorTokens.get(astroColorNameWithTheme);
-    const pass = astroColor?.id === astroColorWithTheme?.id ? true : false;
-    const message = pass
-      ? `Node is using a fill style (${astroColor.name}) from Astro but it's not the correct theme (${theme})`
-      : `Node is using a fill style (${astroColor.name}) from Astro in the correct theme (${theme})`;
+    let pass = false;
+    let message = "";
+    if (astroColor?.name) {
+      const astroColorNameWithTheme = `${theme}/${astroColor?.name}`;
+      const astroColorWithTheme = colorTokens.get(astroColorNameWithTheme);
+      pass = astroColor?.id === astroColorWithTheme?.id ? true : false;
+      message = pass
+        ? `Node is using a fill style (${astroColor.name}) from Astro but it's not the correct theme (${theme})`
+        : `Node is using a fill style (${astroColor.name}) from Astro in the correct theme (${theme})`;
+    } else {
+      message = `Node is not using a fill style from Astro`;
+    }
     const result: LintingResult = {
       test,
       pass,

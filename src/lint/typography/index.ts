208bf8e3d27ca11c73c435369a3755188f8da36a
Gwen Wendell <gwen@thecodingdesigner.com>
2025-06-16 16:19:11 -0400

    Adds typography linting utility with theme validation
    Introduces a utility function to test typography in nodes, including
    validations for type style usage within components. Supports checks
    for correct Astro type styles and theme-specific styles, improving
    linting accuracy and consistency.

---
+++ src/lint/typography/index.ts
@@ -0,0 +1,37 @@
+import { addResult } from "../collect-data/results";
+import { AstroTheme } from "../../types/tokens";
+import { TestableNode } from "../../types/figma";
+import { LintingResult } from "../../types/results";
+import { usingTypeFromComponent } from "./tests/using-type-from-component";
+
+const testTypography = async (
+  node: TestableNode,
+  theme: AstroTheme
+): Promise<void> => {
+  const typographyTestPromises: Promise<LintingResult>[] = [];
+
+  // Fail if node is in a component and not using the correct type style
+  typographyTestPromises.push(usingTypeFromComponent(node));
+  // Fail if node is not in an Astro component,
+  // IS using a type style,
+  // AND not using an Astro type style
+  // typographyTestPromises.push(usingAstroType(node));
+
+  // Fail if node is using an Astro type style but not the correct one for this theme
+  // typographyTestPromises.push(astroTypeIsUsingCorrectTheme(node, theme));
+
+  await Promise.all(typographyTestPromises)
+    .then((results) => {
+      results.forEach((result: LintingResult | undefined) => {
+        if (result) {
+          addResult(result);
+        }
+      });
+      return Promise.resolve();
+    })
+    .catch((error) => {
+      console.error("Error in testColors:", error);
+    });
+};
+
+export { testTypography };

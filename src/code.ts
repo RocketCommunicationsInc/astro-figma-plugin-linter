5534a5b5a1f60a8ad0d080ee08a7f350dddcd435
Gwen Wendell <gwen@thecodingdesigner.com>
2025-06-30 16:31:50 -0400

    Adjusts UI width to improve layout consistency
    Updates the UI width from 500 to 550 to enhance visual alignment and ensure better usability for the plugin interface. This change aims to improve the overall user experience.

--- src/code.ts
+++ src/code.ts
@@ -1,7 +1,7 @@
 import { lintSelection } from "./lint";
 import { AstroTheme } from "./types/tokens";
 
-figma.showUI(__html__, { themeColors: true, width: 500, height: 700 });
+figma.showUI(__html__, { themeColors: true, width: 550, height: 700 });
 
 // Calls to "parent.postMessage" from within the HTML page will trigger this
 // callback. The callback will be passed the "pluginMessage" property of the

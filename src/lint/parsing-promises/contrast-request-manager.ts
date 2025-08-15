fc09f8796ed0bc334492dc071992ca77f108cfe8
Gwen Wendell <gwen@thecodingdesigner.com>
2025-08-15 17:31:23 -0400

    Refactor contrast request management to improve clarity and consistency in naming conventions

--- src/lint/parsing-promises/contrast-request-manager.ts
+++ src/lint/parsing-promises/contrast-request-manager.ts
@@ -1,34 +1,25 @@
-import { LintingResult } from "../../types/results";
-
 type ResolveFn<T> = (value: T | PromiseLike<T>) => void;
 type RejectFn = (reason?: any) => void;
 
-interface PendingRequest<T> {
+interface PendingContrastRequest<T> {
   resolve: ResolveFn<T>;
   reject: RejectFn;
   timer: number;
 }
 
-const pendingRequests = new Map<string, PendingRequest<any>>();
-const REQUEST_TIMEOUT = 5000; // 5 seconds
+const pendingContrastRequests = new Map<string, PendingContrastRequest<any>>();
 
-export function createRequest<T>(requestId: string): Promise<T> {
+export function createContrastRequest<T>(requestId: string): Promise<T> {
   return new Promise<T>((resolve, reject) => {
-    // const timer = window.setTimeout(() => {
-    //   pendingRequests.delete(requestId);
-    //   reject(new Error(`Request '${requestId}' timed out.`));
-    // }, REQUEST_TIMEOUT);
-
-    pendingRequests.set(requestId, { resolve, reject, timer: 0 });
+    pendingContrastRequests.set(requestId, { resolve, reject, timer: 0 });
   });
 }
 
-export function resolveRequest<T>(requestId: string, data: T) {
-  const request = pendingRequests.get(requestId);
-  console.log('requestId', requestId)
+export function resolveContrastRequest<T>(requestId: string, data: T) {
+  const request = pendingContrastRequests.get(requestId);
   if (request) {
     clearTimeout(request.timer);
     request.resolve(data);
-    pendingRequests.delete(requestId);
+    pendingContrastRequests.delete(requestId);
   }
 }

6e687e7f4a88ee947c47f7978109f038714f4312
Gwen Wendell <gwen@thecodingdesigner.com>
2025-08-20 11:04:16 -0400

    Remove unnecessary comments in lintSingleNode function for improved code clarity

--- src/lint/parsing-promises/lint-single.node.ts
+++ src/lint/parsing-promises/lint-single.node.ts
@@ -8,13 +8,13 @@ const lintSingleNode = async (
   node: TestableNode,
   theme: AstroTheme
 ): Promise<void> => {
-  // Test paint style
+
   await testColors(node, theme);
-  // Test typography
   if (node.type === "TEXT") {
     await testTypography(node);
     await testContrast(node);
   }
+  
 };
 
 export { lintSingleNode };
